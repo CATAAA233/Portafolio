b897d01a169aab917191d8d3dc5e78acc649b134
CATAAA233 <carlosdanielcatalan1o@gmail.com>
2023-05-30 17:10:16 -0700

    Cover page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import { Inter } from "next/font/google";
+import styles from "./Home.module.css";
+import Image from "next/image";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-
+      <section className={styles.CoverPage}>
+        <div className={styles.BlackLayer} />
+        <div className={styles.TextContainer}>
+          <p className={styles.Presentation}>Hello, My Name is</p>
+          <p className={styles.Name}>Daniel Catalan</p>
+          <p className={styles.Job}>Full Stack Developer</p>
+          <button className={styles.Button}>Know More</button>
+        </div>
+        <div className={styles.ImageContainer}>
+          <Image
+            fill={true}
+            src={"/images/Home/Profile.jpeg"}
+            alt={"Profile Image"}
+          />
+        </div>
+      </section>
     </main>
-  )
+  );
 }
